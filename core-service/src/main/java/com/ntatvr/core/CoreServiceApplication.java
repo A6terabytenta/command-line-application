1ecb7898402e5aa21ff8d164e7ba322c9cd5a688
ANT <anh.nguyen.tuan@pycogroup.com>
2021-04-28 22:22:03 +0700

    Build Project Structure

---
+++ core-service/src/main/java/com/ntatvr/core/CoreServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ntatvr.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("PMD")
+@SpringBootApplication
+public class CoreServiceApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(CoreServiceApplication.class, args);
+  }
+
+}
